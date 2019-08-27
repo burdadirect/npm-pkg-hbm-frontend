081170a610f63310c00383d75f8fa9735314e9bb
Christian Puchinger <puchinger@burda.com>
2019-08-27 23:21:23 +0200

    Bugfix.

--- js/hbm-bn-plugins.js
+++ js/hbm-bn-plugins.js
@@ -78,7 +78,7 @@ jQuery.fn.hbm_initCollapsibleCards = function () {
     $element.on('click', '[data-card-collapsible-toggle]', function (event) {
       event.preventDefault();
 
-      jQuery(this).closest('[data-card-collapsible]').find('> .card-body.hbm-form-body').not('.hbm-form-header-text').toggle();
+      jQuery(this).closest('[data-card-collapsible]').find('> .card-body.hbm-form-body.hbm-form-body-collapsible').toggle();
       jQuery(this).find('.fa-chevron-up,.fa-chevron-down').toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
     });
   });
